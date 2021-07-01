193c8db9a37a0d7f65f6b790cd69026a4641bdb2
me <me@me.me.com>
2021-07-01 13:57:24 -0700

    env

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { firestoreReducer } from 'redux-firestore';
+
+import { combineReducers } from 'redux';
+
+
+const rootReducer = combineReducers({
+
+  firestore: firestoreReducer
+});
+
+export default rootReducer;
