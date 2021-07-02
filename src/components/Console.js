f278f6f3384cc827e2393ba8e6469d7f576c6186
me <me@me.me.com>
2021-07-02 16:00:23 -0700

    work on displaying room options

--- src/components/Console.js
+++ src/components/Console.js
@@ -1,12 +1,18 @@
+import {useState} from 'react';
 import Display from './Display'
 
 function Console(){
+  let [command,changeCommand] = useState("none");
+  function sendCommand(){
+    command = document.getElementById("command").value;
+    changeCommand(command)
+  }
 
   return(
     <div>
-      <Display/>
-      <input type="text"/>
-      <button>Enter</button>
+      <Display command={command}/>
+      <input id="command" type="text"/>
+      <button onClick={sendCommand}>Enter</button>
     </div>
   )
 }
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,32 +1,89 @@
-import React,{useState} from 'react'
+import React, { useState,useEffect } from 'react'
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
 import { useFirestoreConnect, isLoaded } from 'react-redux-firebase';
 
 const DisplayStyle = styled.div`
   font-size: 24px;
+  font-family: Consolas;
   text-align: left;
   background-color: black;
   padding: 10px;
   color: green;
-  width: 300px;
-  height: 300px;
+  width: 400px;
+  height: 400px;
 `;
 
 
-function Display(){
+
+
+function Display(props) {
   const [room, changeRoom] = useState("X61whV3TLafhIwdZrees")
 
+
+  useEffect((prevProps)=>
+  {
+
+  })
+
   useFirestoreConnect([
-    { collection: 'room', doc: room, storeAs: "room" }
+    { collection: 'rooms', storeAs: "rooms" }
   ]);
 
-  const room = useSelector(state => state.firestore.ordered["quizzes"])
+  const rooms = useSelector(state => state.firestore.ordered["rooms"])
 
-  return (
-   <DisplayStyle>
-     
-   </DisplayStyle>
-  )
+  function getAdjacentRooms(current) {
+    console.log(current,"current")
+    let obj = {
+      left: rooms.find(x => x.id === current.left),
+      right: rooms.find(x => x.id === current.right),
+      forward: rooms.find(x => x.id === current.forward),
+      backward: rooms.find(x => x.id === current.backward),
+      up: rooms.find(x => x.id === current.up),
+      down: rooms.find(x => x.id === current.down)
+    }
+    return obj;
+  }
+
+  function respondToCommandFromProps(current){
+    if (getAdjacentRooms(current)[props.command].id !== undefined && getAdjacentRooms(current)[props.command].id !== room) {
+      changeRoom(current[props.command].id)
+    }
+  }
+ 
+
+  function displayRooms(current) {
+
+    let obj = getAdjacentRooms(current);
+    let display = "\n";
+    Object.keys(obj).forEach(x => {
+      if (obj[x] !== undefined) {
+        display += "To your " + x + " is the " + obj[x].name + ".\n"
+      }
+    }
+    )
+    return display;
+  }
+
+  if (isLoaded(rooms) && rooms !== undefined && rooms?.length) {
+    let current = rooms.find(x => x.id === room);
+    respondToCommandFromProps();
+    
+    return (
+      <DisplayStyle>
+        {current.message}
+        {displayRooms(current)}
+      </DisplayStyle>
+    )
+  }
+  else {
+    return (
+      <DisplayStyle>
+
+      </DisplayStyle>
+
+    )
+  }
 }
 
 export default Display;
